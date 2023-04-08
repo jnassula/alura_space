29de9ea47d5c5e9b270430c78dc56f5a7939e7c5
jnassula <jonatanassula@gmail.com>
2023-04-08 20:43:05 +0100

    studying django

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from galeria.views import index, imagem
 
-
 urlpatterns = [
     path('', index, name='index'),
-    path('imagem/', imagem, name='imagem'),
+    path('imagem/<int:foto_id>', imagem, name='imagem'),
 ]
